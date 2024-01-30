126376205a59504adf830f7972ba55c8ed177257
Bain Kennedy <bain@tsxentertainment.com>
2024-01-30 11:58:16 -0800

    Adding project settings.

---
+++ src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       bainfu                                                       #
+# 	Created:      1/27/2024, 8:29:11 PM                                        #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
